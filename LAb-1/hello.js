c51ad0bc36aa56280b4483daf19ac201db46165c
aditya-upadhyay <upadhayayaditya710@gmail.com>
2026-07-23 11:54:08 +0530

    initial commit

---
+++ LAb-1/hello.js
@@ -0,0 +1,14 @@
+console.log("Hello, World!");
+console.log("aditya upadhyay");
+function sum(a, b) {
+    return a + b;
+}
+
+const add = (a, b) => {
+     return a + b;
+};
+const add2 = (a, b) => a + b;
+
+console.log(sum(2, 3));
+console.log(add(5, 7));
+console.log(add2(10, 15));
